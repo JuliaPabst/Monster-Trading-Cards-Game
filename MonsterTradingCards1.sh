d4425a5065d0db2759aa345bc259d5da0848fdf1
Jules Pabst <julia.pabst@kununu.com>
2024-12-06 11:50:58 +0100

    add specification

--- MonsterTradingCards1.sh
+++ MonsterTradingCards1.sh
@@ -20,55 +20,23 @@ done
 if [ $pauseFlag -eq 1 ]; then read -p "Press enter to continue..."; fi
 
 # --------------------------------------------------
-echo "1) Create Users (Registration)"
-# Create User
-curl -i -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"daniel\"}"
+echo "4) acquire packages kienboec"
+curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer kienboec-mtcgToken" -d ""
 echo "Should return HTTP 201"
 echo .
-curl -i -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"altenhof\", \"Password\":\"markus\"}"
+curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer kienboec-mtcgToken" -d ""
 echo "Should return HTTP 201"
 echo .
-curl -i -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"admin\",    \"Password\":\"istrator\"}"
+curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer kienboec-mtcgToken" -d ""
 echo "Should return HTTP 201"
 echo .
-
-if [ $pauseFlag -eq 1 ]; then read -p "Press enter to continue..."; fi
-
-echo "should fail:"
-curl -i -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"daniel\"}"
-echo "Should return HTTP 4xx - User already exists"
+curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer kienboec-mtcgToken" -d ""
+echo "Should return HTTP 201"
 echo .
-curl -i -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"different\"}"
-echo "Should return HTTP 4xx - User already exists"
+echo "should fail (no money):"
+curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer kienboec-mtcgToken" -d ""
+echo "Should return HTTP 4xx - Not enough money"
 echo .
 echo .
 
 if [ $pauseFlag -eq 1 ]; then read -p "Press enter to continue..."; fi
-
-# --------------------------------------------------
-echo "2) Login Users"
-curl -i -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"daniel\"}"
-echo "should return HTTP 200 with generated token for the user, here: kienboec-mtcgToken"
-echo .
-curl -i -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"altenhof\", \"Password\":\"markus\"}"
-echo "should return HTTP 200 with generated token for the user, here: altenhof-mtcgToken"
-echo .
-curl -i -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"admin\",    \"Password\":\"istrator\"}"
-echo "should return HTTP 200 with generated token for the user, here: admin-mtcgToken"
-echo .
-
-if [ $pauseFlag -eq 1 ]; then read -p "Press enter to continue..."; fi
-
-echo "should fail:"
-curl -i -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"different\"}"
-echo "Should return HTTP 4xx - Login failed"
-echo .
-echo .
-
-if [ $pauseFlag -eq 1 ]; then read -p "Press enter to continue..."; fi
-
-# --------------------------------------------------
-echo "3) create packages (done by admin)"
-curl -i -X POST http://localhost:10001/packages --header "Content-Type: application/json" --header "Authorization: Bearer admin-mtcgToken" -d "[{\"Id\":\"845f0dc7-37d0-426e-994e-43fc3ac83c08\", \"Name\":\"WaterGoblin\", \"Damage\": 10.0}, {\"Id\":\"99f8f8dc-e25e-4a95-aa2c-782823f36e2a\", \"Name\":\"Dragon\", \"Damage\": 50.0}, {\"Id\":\"e85e3976-7c86-4d06-9a80-641c2019a79f\", \"Name\":\"WaterSpell\", \"Damage\": 20.0}, {\"Id\":\"1cb6ab86-bdb2-47e5-b6e4-68c5ab389334\", \"Name\":\"Ork\", \"Damage\": 45.0}, {\"Id\":\"dfdd758f-649c-40f9-ba3a-8657f4b3439f\", \"Name\":\"FireSpell\",    \"Damage\": 25.0}]"
-echo "Should return HTTP 201"
-echo .
